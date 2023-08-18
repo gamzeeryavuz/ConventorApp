9b8ed7208618db3636c0cbf15c36151364d87581
gamzeeryavuz <115155686+gamzeeryavuz@users.noreply.github.com>
2023-08-18 20:42:43 +0300

    Convertor App
    Weight transformation programme

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button btn;
+    TextView textView;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText=findViewById(R.id.editText);
+        btn=findViewById(R.id.btn) ;
+        textView =findViewById(R.id.textview);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Getting the user input(Kilos)
+                String inputText=editText.getText().toString();
+
+                // Converting a string into double
+
+                double kilos=Double.parseDouble(inputText);
+
+
+                // Converting kilos into pounds
+                double pounds =makeConversion(kilos);
+
+                textView.setText(""+pounds);
+
+            }
+        });
+    }
+
+    public double makeConversion(double kilos){
+        // 1kilo =2.20462 pounds
+
+        return kilos *2.20462;
+
+    }
+}
